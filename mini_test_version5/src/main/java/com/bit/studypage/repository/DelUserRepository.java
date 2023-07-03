0eeb5a26e549fc8de2b10a429ca98d9324146dc9
kjeongw00 <roekfo333@naver.com>
2023-07-03 17:16:02 +0900

    delete all

--- mini_test_version5/src/main/java/com/bit/studypage/repository/DelUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bit.studypage.repository;
-
-import com.bit.studypage.entity.DelUsers;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DelUserRepository extends JpaRepository<DelUsers,Long> {
-}
--- mini_test_version5/src/main/java/com/bit/studypage/service/ProfileImgService.java
+++
@@ -1,8 +0,0 @@
-package com.bit.studypage.service;
-
-import com.bit.studypage.entity.ProfileImg;
-
-public interface ProfileImgService {
-    public void updateProfileImg(ProfileImg profileImg);
-    public ProfileImg getProfileImg(Long userId);
-}
